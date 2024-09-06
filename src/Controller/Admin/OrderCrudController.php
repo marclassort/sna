05b80f5240cb398311d69c533af1409ee9158a87
marc <mlassort@acfi-network.com>
2024-09-06 19:58:33 +0200

    Commit all files SKK.

---
+++ src/Controller/Admin/OrderCrudController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Order;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Form\Type\FileUploadType;
+
+class OrderCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Order::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Commande')
+            ->setEntityLabelInPlural('Commandes')
+            ->setPageTitle(Crud::PAGE_INDEX, 'Gestion des commandes');
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->hideOnForm(),
+            TextField::new('orderType', 'Type de commande'),
+            ChoiceField::new('status', 'Statut')
+                ->setChoices([
+                    "En cours" => "en-cours",
+                    "Payée" => "payee",
+                    "Annulée" => "annulee"
+                ]),
+            MoneyField::new('totalAmount', 'Montant total')
+                ->setCurrency("EUR")
+                ->setNumDecimals(2)
+                ->formatValue(function ($value) {
+                    return $value . " €";
+                }),
+            DateField::new('createdAt', 'Commande passée le')
+                ->formatValue(function ($value) {
+                    return $value->format("d/m/Y à H:i");
+                })
+        ];
+    }
+}
